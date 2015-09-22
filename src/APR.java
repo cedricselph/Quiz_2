e1948f574320a5f5260e3149d503cc6f040c51c5
Cedric Selph <selphc@udel.edu>
2015-09-21 21:53:22 -0400

    Update

--- src/APR.java
+++ src/APR.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import org.apache.poi.ss.formula.functions.FinanceLib;
 
 public class APR {
 
@@ -11,7 +12,7 @@ public class APR {
 		System.out.print("Percent increaser per year:");
 		double percent_increase=input.nextDouble();
 		
-		System.out.print("Fixed loan APR");
+		System.out.print("Fixed loan APR:");
 		double APR= input.nextDouble();
 		
 		System.out.print("How many years to pay off?:");
@@ -19,13 +20,30 @@ public class APR {
 	    input.close();
 		
 		
-	    double sum = tuition_start;
+	    double total_cost = tuition_start;
+	    double tuition=tuition_start;
 		
 	    /** I am pretty sure i am setting up the for loop up properly but it will not execute.
 	    */
-		for(int years=0; (years==4);years+=1 ){
-			sum+=(tuition_start*(1+percent_increase));
-			System.out.print(sum);}
+		for(int years=0; years<=2 ;years+=1 ){
+			
+			tuition=tuition*(1+percent_increase);
+			total_cost+=tuition;
+			System.out.print(total_cost + "\n");}
+		/**This section provides the monthly payments
+		 */
+		
+		double PMT;
+		
+		double r_2=((APR)/12);
+		double n_2=(schedule*12);
+		double p_2=0;
+		double f_2=total_cost;
+		boolean t_2=false;
+		
+		PMT=FinanceLib.pmt(r_2, n_2, p_2, f_2, t_2);
+		
+		System.out.print(PMT);
 		
 		
 		
